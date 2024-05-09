d5b1ed4c0e6c16c33984f623681cf5fb8aa94867
Moeein <moeeeinaali@gmail.com>
2024-05-09 17:14:36 +0330

    Add Assignment03-Question04 Solution

--- Assignments/Assignment-03/Solutions/problem-04/queries.sql
+++ Assignments/Assignment-03/Solutions/problem-04/queries.sql
@@ -1,4 +1,32 @@
 -- Query 1
+SELECT books.title, SUM(Orders.Quantity) AS Sum
+FROM orders
+         JOIN books ON orders.bookid = books.bookid
+GROUP BY books.title
+ORDER BY Sum DESC
+LIMIT 3;
 -- Query 2
+SELECT customers.name, SUM(books.price * orders.quantity) AS TotalPrice
+FROM customers
+         JOIN orders ON customers.customerid = orders.orderid
+         JOIN books ON orders.bookid = books.bookid
+GROUP BY customers.customerid
+ORDER BY TotalPrice DESC
+LIMIT 1;
 -- Query 3
+SELECT books.genre, SUM(quantity) AS Sum, avg(price) AS Average
+FROM books
+         JOIN orders ON books.bookid = orders.orderid
+WHERE orders.orderdate >= '2023-01-01'
+GROUP BY books.genre;
 -- Query 4
+SELECT Books.Title,
+       Books.Genre,
+       AVG(Reviews.Rating) AS Average,
+       RANK() OVER (
+           PARTITION BY Books.Genre ORDER BY AVG(Reviews.Rating) DESC
+           )               AS Rank
+FROM books
+         FULL JOIN reviews ON books.bookid = reviews.reviewid
+GROUP BY Books.Title, Books.Genre
+ORDER BY Books.Genre, Average DESC;
